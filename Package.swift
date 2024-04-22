a152915031c7580ee7c3442978bc74729b80bdb0
Davidbilly Billy <davidbilly@2c2p.com>
2024-04-22 16:20:20 +0800

    - Update sdk url path.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PGW",
-            url:"https://2c2p-cloudfront.s3.ap-southeast-1.amazonaws.com/devPortal/demoCode/Mobile/PGW_SDK_iOS_v4.6.0.zip",
+            url:"https://2c2p-cloudfront.s3.ap-southeast-1.amazonaws.com/devPortal/demoCode/Mobile/PGW_SDK/PGW/PGW_SDK_iOS_v4.6.0.zip",
             checksum: "1eb1fec3cebec3c44ccb51bc98ec778496ec17c8d6e5bb886e8b18917b902250"
         )
     ]
