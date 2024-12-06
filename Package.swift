7d0f9a8573cffd978fc747a4641a6ae06e26dff3
Davidbilly Billy <davidbilly@2c2p.com>
2024-12-06 10:49:35 +0800

    - Update for PGW SDK v4.7.1.

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PGW",
-            url:"https://2c2p-cloudfront.s3.ap-southeast-1.amazonaws.com/devPortal/demoCode/Mobile/PGW_SDK/PGW/PGW_SDK_iOS_v4.7.0.zip",
-            checksum: "1bca1cb5df9df84232419b0a99106d974ab646d1ff9beee6657b9236a99ab6b1"
+            url:"https://d27uu9vmlo4gwh.cloudfront.net/PGW_SDK_iOS_v4.7.1/PGW_SDK_iOS_v4.7.1.zip",
+            checksum: "7e02f50a19031a084074ea264e78c38a08fcb409e6dd2e934629e1457f16567f"
         )
     ]
 )
